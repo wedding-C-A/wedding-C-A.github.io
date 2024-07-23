98abb7e6bd606b80bd309afcb6df028097f44560
ayako0802 <ayakof0802@gmail.com>
2024-07-23 22:38:58 +0900

    feat: add Name

---
+++ src/components/Name.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+const Name = () => {
+    return (
+        <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+            Name
+        </Box>
+    );
+}
+
+export default Name
