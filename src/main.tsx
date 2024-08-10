3211e6663d9770168aa5015bcf91c7e251035df7
Doyage <pcstt@naver.com>
2024-08-11 01:11:47 +0900

    fix: og 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import imgSrc from '/assets/od0002.jpg';
-// import imgSrc from '/assets/countdown.jpg';
-
-document
-  .querySelector('meta[property="og:image"]')
-  ?.setAttribute('content', imgSrc);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
