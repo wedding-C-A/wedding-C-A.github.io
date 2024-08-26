814ce98a5d0fb893c6a2ce34aa729c557b94486e
Doyage <pcstt@naver.com>
2024-08-26 23:11:24 +0900

    fix: pinch zoom 막기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Box, Container, Paper } from '@mui/material';
-import { useEffect } from 'react';
 import './App.css';
 import Account from './components/Account';
 import Contact from './components/Contact';
@@ -11,16 +10,6 @@ import Map from './components/Map';
 import Sound from './components/Sound';
 
 const App = () => {
-  useEffect(() => {
-    const metaViewport = document.querySelector('meta[name="viewport"]');
-    if (metaViewport) {
-      metaViewport.setAttribute(
-        'content',
-        'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
-      );
-    }
-  }, []);
-
   return (
     <Container maxWidth="md">
       <Sound />
